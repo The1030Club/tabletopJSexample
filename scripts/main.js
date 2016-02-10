a7759185835c8036ac4c758fe0fc9ce30d12e0e2
Ben Haaland <bphaaland@gmail.com>
2016-02-09 20:59:31 -0500

    adding a display users function and removing styles

--- scripts/main.js
+++ scripts/main.js
@@ -15,30 +15,23 @@ function showInfo(data, tabletop) {
     }
 }
 
+function displayInfo() {
+    var firstName =           userData[0].last_name;
+    var lastName =            userData[0].first_name;
+    var houseAddress =        userData[0].home_address;
+    var userId =              userData[0].user_id;
+    var homeWalkthroughDate = userData[0].home_walkthrough_date;
+    var homeImg =             userData[0].home_img;
+    var homeSqFt =            userData[0].home_sq_feet;
+    var homeAge =             userData[0].home_age;
+    var utilitySuppliers =    userData[0].utility_suppliers;
 
-
-
-
-//var firstName =           userData[0].Object.last_name;
-//var lastName =            userData[0].first_name;
-//var houseAddress =        userData[0].home_address;
-//var userId =              userData[0].user_id;
-//var homeWalkthroughDate = userData[0].home_walkthrough_date;
-//var homeImg =             userData[0].home_img;
-//var homeSqFt =            userData[0].home_sq_feet;
-//var homeAge =             userData[0].home_age;
-//var utilitySuppliers =    userData[0].utility_suppliers;
-//
-//
-//function displayInfo() {
-//    document.getElementById('js-userFirstName').innerHTML       = firstName;
-//    document.getElementById('js-userLastName').innerHTML        = lastName;
-//    document.getElementById('js-userAddress').innerHTML         = houseAddress;
-//    document.getElementById('js-homeWalkthroughDate').innerHTML = homeWalkthroughDate;
-//    document.getElementById('js-homeImg').src                   = "img/" + homeImg;
-//    document.getElementById('js-homeSqFt').innerHTML            = homeSqFt;
-//    document.getElementById('js-homeAge').innerHTML             = homeAge;
-//    document.getElementById('js-utilitySuppliers').innerHTML    = utilitySuppliers;
-//}
-//
-//displayInfo();
+    document.getElementById('js-userFirstName').innerHTML       = firstName;
+    document.getElementById('js-userLastName').innerHTML        = lastName;
+    document.getElementById('js-userAddress').innerHTML         = houseAddress;
+    document.getElementById('js-homeWalkthroughDate').innerHTML = homeWalkthroughDate;
+    document.getElementById('js-homeImg').src                   = "img/" + homeImg;
+    document.getElementById('js-homeSqFt').innerHTML            = homeSqFt;
+    document.getElementById('js-homeAge').innerHTML             = homeAge;
+    document.getElementById('js-utilitySuppliers').innerHTML    = utilitySuppliers;
+}
